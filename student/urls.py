4a0bb88d0b97902716aabc8a24bb0314d7095278
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2024-09-18 21:56:32 +0500

    error is resolved

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,14 @@
+# urls.py in your Django app
 from django.urls import path
-from .views import StudentListCreate
+from .views import *
+
+# urlpatterns = [
+#     path('add_student/', StudentView.as_view(), name='student_api'),  # URL for the API view
+#     path('students/', student_list, name='student_list'),  # URL for the HTML rendering view
+# ]
+from django.urls import path
+from .views import AddStudentView
 
 urlpatterns = [
-    path('students/', StudentListCreate.as_view(), name='student-list-create'),
+    path('add_student/', AddStudentView.as_view(), name='add_student'),
 ]
